39ddf7d0c40aac655db10da295388e18e85b1a4b
vovakpro13 <vovakpro13@gmail.com>
2021-07-06 21:39:50 +0300

    JWT (logIn, logOut, refresh)

--- services/auth.service.js
+++ services/auth.service.js
@@ -5,6 +5,7 @@ module.exports = {
     createTokens: async (userId) => {
         const tokens = tokenService.generateTokens();
 
+        await Token.deleteMany({ user: userId });
         await Token.create({ user: userId, ...tokens });
 
         return tokens;
